e67c651ea30d07deb83932e405d62339d817ce56
Unknown <kurupical@gmail.com>
2019-09-22 14:20:16 +0900

    TEST3: nanpatternをより細かく

--- src/feature2/f_110_nan_pattern.py
+++ src/feature2/f_110_nan_pattern.py
@@ -16,7 +16,17 @@ def make_nan_pattern(df):
     df["nan_pattern_C"] = df[["C{}".format(x) for x in range(1, 14+1)]].isnull().astype(int).apply(join_string, axis=1)
     df["nan_pattern_D"] = df[["D{}".format(x) for x in range(1, 15+1)]].isnull().astype(int).apply(join_string, axis=1)
     df["nan_pattern_M"] = df[["M{}".format(x) for x in range(1, 9+1)]].isnull().astype(int).apply(join_string, axis=1)
-    df["nan_pattern_V"] = df[["V{}".format(x) for x in range(1, 339+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V1_11"] = df[["V{}".format(x) for x in range(1, 11+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V12_34"] = df[["V{}".format(x) for x in range(12, 34+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V35_52"] = df[["V{}".format(x) for x in range(35, 52+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V53_74"] = df[["V{}".format(x) for x in range(53, 74+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V75_94"] = df[["V{}".format(x) for x in range(75, 94+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V95_137"] = df[["V{}".format(x) for x in range(95, 137+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V138_166"] = df[["V{}".format(x) for x in range(138, 166+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V167_216"] = df[["V{}".format(x) for x in range(167, 216+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V217_278"] = df[["V{}".format(x) for x in range(217, 278+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V279_321"] = df[["V{}".format(x) for x in range(279, 321+1)]].isnull().astype(int).apply(join_string, axis=1)
+    df["nan_pattern_V322_339"] = df[["V{}".format(x) for x in range(322, 339+1)]].isnull().astype(int).apply(join_string, axis=1)
     df["nan_pattern_M+M4"] = df["nan_pattern_M"].astype(str).str.cat(df["M4"].astype(str))
     return df
 
